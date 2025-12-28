e2a97b8da4aa07732495b6a5f49d580f42106b89
Antoinette <inerchild2000@gmail.com>
2025-12-28 13:40:24 -0500

    "Added UI"

---
+++ nourishnet-backend/src/main/java/nourishnet/NourishnetBackendApplication.java
@@ -0,0 +1,13 @@
+package nourishnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NourishnetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NourishnetBackendApplication.class, args);
+	}
+
+}
---
+++ nourishnet-backend/src/test/java/nourishnet/NourishnetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nourishnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NourishnetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
